8f33f291897f14b860e033a19c799493fd21cd3b
Patrick Nikoletich <pnikoletich@gmail.com>
2018-07-08 16:21:33 -0700

    Fix bundling issue

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -3,6 +3,9 @@ module.exports = {
       return require.resolve('react-native-typescript-transformer');
     },
     getSourceExts() {
-      return ['ts', 'tsx', 'pb', 'txt'];
+      return ['ts', 'tsx'];
+    },
+    getAssetExts() {
+      return ['pb', 'txt']
     }
   }
--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { WelcomeScreen } from './screens/welcomeScreen';
-import { BuildScreen } from './screens/buildScreen';
-import { TestScreen } from './screens/testScreen';
-import { DistributeScreen } from './screens/distributeScreen';
-import { CrashesScreen } from './screens/crashesScreen';
-import { AnalyticsScreen } from './screens/analyticsScreen';
-import { PushScreen } from './screens/pushScreen';
 import { CodePushScreen } from './screens/codePushScreen';
 
 const RootTabNavigator = TabNavigator(
